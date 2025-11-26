2ed92e1d359c19667fdf00eb3e1b2e6f148906cc
Garry Ryan <garry.ryan01@gmail.com>
2025-11-26 02:41:00 +0000

    Here are visual examples of what I want for the polygon button

--- src/components/icons/PolygonIcon.tsx
+++ src/components/icons/PolygonIcon.tsx
@@ -2,19 +2,31 @@ import * as React from "react"
 import { SVGProps } from "react"
 
 export const PolygonIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    {...props}
-  >
-    <path d="M5.5 19.5 19 21l-3-8.5-9.5 4Z" />
-    <path d="m19 21 1-10-8-4.5-8 4.5 5 10" />
-  </svg>
+    <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        width="24" 
+        height="24" 
+        viewBox="0 0 24 24" 
+        fill="none" 
+        stroke="currentColor" 
+        strokeWidth="2.5" 
+        strokeLinecap="round" 
+        strokeLinejoin="round" 
+        {...props}
+    >
+        <path d="M13.5 3.5h-5l-4 6.5 4 6.5h5l4-6.5Z"/>
+        <circle cx="13.5" cy="3.5" r="1.5" fill="currentColor" stroke="none"/>
+        <circle cx="8.5" cy="3.5" r="1.5" fill="currentColor" stroke="none"/>
+        <circle cx="4.5" cy="10" r="1.5" fill="currentColor" stroke="none"/>
+        <circle cx="8.5" cy="16.5" r="1.5" fill="currentColor" stroke="none"/>
+        <circle cx="13.5" cy="16.5" r="15" fill="currentColor" stroke="none"/>
+        <circle cx="17.5" cy="10" r="1.5" fill="currentColor" stroke="none"/>
+        
+        <circle cx="13.5" cy="3.5" r="1.5" fill="currentColor" stroke="none"/>
+        <circle cx="8.5" cy="3.5" r="1.5" fill="currentColor" stroke="none"/>
+        <circle cx="4.5" cy="10" r="1.5" fill="currentColor" stroke="none"/>
+        <circle cx="8.5" cy="16.5" r="1.5" fill="currentColor" stroke="none"/>
+        <circle cx="13.5" cy="16.5" r="1.5" fill="currentColor" stroke="none"/>
+        <circle cx="17.5" cy="10" r="1.5" fill="currentColor" stroke="none"/>
+    </svg>
 )
